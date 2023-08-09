2408f2f5be13a9aa728839d1a5a6cdb78273d504
bala-09 <134393829+bala-09@users.noreply.github.com>
2023-08-09 15:03:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>HMS</title>
+    <title>HOSTEL MANAGEMENT SYATEM</title>
     <!-- meta tags -->
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
