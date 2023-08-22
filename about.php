199ab39336afa971ab263dcbd0cee8dd688f5af7
bala-09 <134393829+bala-09@users.noreply.github.com>
2023-08-22 16:37:02 +0530

    Update about.php

--- about.php
+++ about.php
@@ -8,7 +8,7 @@
 <html lang="en">
 <head>
 <title> Intrend Interior Category Flat Bootstrap Responsive Website Template | About : W3layouts</title>
-<!-- Hello text	new 1234-->
+<!-- Hello-->
 	<!-- Meta tag Keywords -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="utf-8">
